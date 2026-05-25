7367080d2a11e5dc2ad6e86986e480ce0d19d18e
parisfry <p.fry0220221@arts.ac.uk>
2026-05-25 11:24:43 +0100

    First upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+const menuToggle = document.getElementById('menuToggle');
+
+const mobileMenu = document.getElementById('mobileMenu');
+
+menuToggle.addEventListener('click', () => {
+
+  mobileMenu.classList.toggle('active');
+
+});
